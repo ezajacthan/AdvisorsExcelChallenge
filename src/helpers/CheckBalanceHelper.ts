db06f3ffbec764c564d17ba7e7de75f3ad156ece
Ethan Zajac <ezajacthan@gmail.com>
2024-04-10 15:42:31 -0500

    Get balance implemented

---
+++ src/helpers/CheckBalanceHelper.ts
@@ -0,0 +1,15 @@
+import {RunQuery, initConnection, closeConnection} from "../utils/Database.js";
+
+export async function CheckBalance(account: number){
+    const getAccountQuery = "Select amount as balance from accounts where account_number = $1";
+    let queryRes;
+    try{
+        queryRes = await RunQuery(getAccountQuery, [account]);
+    } catch (err){
+        //Could add more expansive error handling here, like writing to an error table, notifying a support team, etc.
+        console.error(`Caught error getting account information for account ${account}`);
+        throw err;
+    }
+    
+    return queryRes.rows[0].balance;
+}
